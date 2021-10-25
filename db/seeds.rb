54b7708827d0416714be7e07430b4f11067b98ad
Jacob <Jacobonasrallah@gmail.com>
2021-10-25 15:51:07 -0700

    completed seed file, seeding next

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,43 +7,43 @@ puts "Fetching some puppers 🐕 "
 
 25.times do
     PetProfile.create(
-        name: 
-        species:
-        breed:
-        age:
-        owner_hobby:
-        image_url:
-        profile_like:
+        name: Faker::Creature::Dog.name
+        species: "Dog"
+        breed: Faker::Creature::Dog.breed
+        age: rand(15)
+        owner_hobby: Faker::Hobby.activity
+        image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: [‘dog’], match_all: true)
+        profile_like: [true, false].sample
     )
 end
 
 
+puts "Begging some cats to join us on the couch 🐈 "
 
 25.times do
     PetProfile.create(
-        name: 
-        species:
-        breed:
-        age:
-        owner_hobby:
-        image_url:
-        profile_like:
+        name: Faker::Creature::Cat.name
+        species: "Cat"
+        breed: Faker::Creature:Cat.breed
+        age: rand(20)
+        owner_hobby: Faker::Hobby.activity
+        image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: [‘cat’], match_all: true)
+        profile_like: [true, false].sample
     )
 end
+
+puts "creating the lucky single user"
+
+MainUser.create(
+    name: Gwen
+    species: "Cat"
+    breed: Faker::Creature:Cat.breed
+    age: rand(20)
+    owner_hobby: "Gardening"
+    image: "https://images.pexels.com/photos/982865/pexels-photo-982865.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&dpr=1"
+)
 
 puts "✅ Done seeding!"
 
 
 
-
-
-
-
-# :name
-# :species 
-# :breed
-# :age
-# :owner_hobby
-# :image_url
-# :user_like
-# :profile_like
